3551f6ac4420deb10b1b7253c87f4c1670447854
WILLIAM ARSENEAULT <1663913@CegepGranby.qc.ca>
2018-12-12 18:27:21 -0500

    Création d'attaque fonctionnelle

---
+++ Laboratoire5.1/ViewsModels/AttaqueInfoVM.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Laboratoire5._1
+{
+    public class AttaqueInfoVM : INotifyPropertyChanged
+    {
+        Attaque attaqueModel;
+
+        private RelayCommand sauvegarderCommand;
+
+        private Dictionary<string, string> errorList;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public event EventHandler DemandeFermeture;
+
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public AttaqueInfoVM()
+        {
+            errorList = new Dictionary<string, string>();
+            errorList["Adresse"] = "";
+
+            attaqueModel = new Attaque();
+        }
+
+        public AttaqueInfoVM(Attaque a)
+        {
+            errorList = new Dictionary<string, string>();
+            errorList["Adresse"] = "";
+
+            attaqueModel = a;
+
+        }
+
+        public string Nom
+        {
+            get
+            {
+                return attaqueModel.Nom;
+            }
+            set
+            {
+                attaqueModel.Nom = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public int Dommage
+        {
+            get
+            {
+                return attaqueModel.Degats;
+            }
+            set
+            {
+                attaqueModel.Degats = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public int Mana
+        {
+            get
+            {
+                return attaqueModel.Mana;
+            }
+            set
+            {
+                attaqueModel.Mana = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public ICommand SauvegarderCommand
+        {
+            get
+            {
+                if (sauvegarderCommand == null)
+                {
+                    sauvegarderCommand = new RelayCommand(Sauvegarder, CanSauvegarder);
+                }
+
+                return sauvegarderCommand;
+            }
+        }
+
+        private bool CanSauvegarder(object o)
+        {
+            foreach (KeyValuePair<string, string> error in errorList)
+            {
+                if (error.Value != "")
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        private void Sauvegarder(object o)
+        {
+            Console.WriteLine("Enregistrer dans la DB.");
+
+            using (Labo5DbContext db = new Labo5DbContext())
+            {
+                //si egal 0, nouvelle maison
+                if (attaqueModel.AttaqueID == 0)
+                {
+                    db.Entry(attaqueModel).State = EntityState.Added;
+
+                    //dbContext.Maisons.Add(maisonModel);
+                }
+                else //si different de 0, maison existante que je modifie
+                {
+                    db.Entry(attaqueModel).State = EntityState.Modified;
+                }
+
+                db.SaveChanges();
+            }
+
+            DemandeFermeture?.Invoke(this, new EventArgs());
+        }
+    }
+}
---
+++ Laboratoire5.1/ViewsModels/PartieInfoVM.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratoire5._1
+{
+    class PartieInfoVM : INotifyPropertyChanged
+    {
+        private Partie partieModel;
+
+        private Dictionary<string, string> errorList;
+
+        private RelayCommand changerCommand;
+        private RelayCommand sauvegarderCommand;
+        private RelayCommand supprimerCommand;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public PartieInfoVM()
+        {
+            errorList = new Dictionary<string, string>();
+            errorList["Adresse"] = "";
+
+            partieModel = new Partie();
+
+            using (Labo5DbContext db = new Labo5DbContext())
+            {
+                partieModel.Personnages = db.Personnages.ToList();
+            }
+        }
+
+        public PartieInfoVM(Partie p)
+        {
+            errorList = new Dictionary<string, string>();
+            errorList["Adresse"] = "";
+
+            partieModel = p;
+
+            using(Labo5DbContext db = new Labo5DbContext())
+            {
+                partieModel.Personnages = db.Personnages.ToList();
+            }
+            
+        }
+
+        public ICollection<Personnage> Personnages
+        {
+            get
+            {
+                return partieModel.Personnages;
+            }
+            set
+            {
+                partieModel.Personnages = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public DateTime Date
+        {
+            get
+            {
+                return partieModel.Date;
+            }
+            set
+            {
+                partieModel.Date = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+}
